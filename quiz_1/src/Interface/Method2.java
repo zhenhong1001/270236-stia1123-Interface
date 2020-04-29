df0e9ab2a7262e1e80397854c960f190a2b4ab74
Zhen Hong <60844852+zhenhong1001@users.noreply.github.com>
2020-04-29 12:36:17 +0800

    Add files via upload

---
+++ quiz_1/src/Interface/Method2.java
@@ -0,0 +1,15 @@
+package Interface;
+
+public class Method2 {
+
+    public static void main(String[] args) {
+
+        MyInterface myInterface = new MyInterface(){
+            @Override
+            public void sayHello() {
+                System.out.println("Hello Method2");
+            }
+        };
+        myInterface.sayHello();
+    }
+}
---
+++ quiz_1/src/Interface/Method3.java
@@ -0,0 +1,8 @@
+package Interface;
+
+public class Method3 {
+    public static void main(String[] args) {
+        MyInterface myInterface = () -> System.out.println("Hello Method3");
+        myInterface.sayHello();
+    }
+}
